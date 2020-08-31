87547492a4807851d7ca0eb24be1ee621ea90faa
Usama <ussama.ahmed05@gmail.com>
2020-09-01 03:15:41 +0500

    Done

--- AsiaTravels/Controllers/HomeController.cs
+++ AsiaTravels/Controllers/HomeController.cs
@@ -8,6 +8,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Threading.Tasks;
 
+
 namespace AsiaTravels.Controllers
 {
 
@@ -37,49 +38,97 @@ namespace AsiaTravels.Controllers
             return View();
         }
 
+
+        //public ActionResult SendEmails(EmailFormModel model)
+        //{
+        //    try { 
+        //    if (ModelState.IsValid)
+        //    {
+        //        MailAddress senderEmail = new MailAddress("musamasood@gmail.com", "Demo");
+        //        var receiverEmail = new MailAddress(receiver, "Receiver");
+        //        var password = "Um9-5Tz)(,5tanx";
+        //            var sub = Name;
+        //            string body = message;
+
+        //            var smtp = new SmtpClient
+        //        {
+        //                Host = "smtp.gmail.com",
+        //            Port = 587,
+        //            EnableSsl = true,
+        //            DeliveryMethod = SmtpDeliveryMethod.Network,
+        //            Credentials = new NetworkCredential(senderEmail.Address, password)
+        //        };
+
+        //            using (var mess = new MailMessage(senderEmail, receiverEmail)
+        //        {
+        //            Subject = (string)subject,
+        //            Body = body,
+        //        })
+        //        {
+        //            smtp.Send(mess);
+        //        }
+        //          ViewBag.Index = "Sucessfully sent";
+        //        return RedirectToAction("Index");
+        //    }
+        //    }
+        //    catch (Exception)
+        //    {
+        //        ViewBag.Error = "Some Error";
+        //        return RedirectToAction("Some Error");
+        //    }
+        //    return View();
+
+
+        //}
+
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult SendEmails(string receiver, string subject, string message)
+        public ActionResult SendEmails(string Name, string FromEmail, string Message, double Phone, string Subject)
         {
-            try { 
-            if (ModelState.IsValid)
+            try
             {
-                MailAddress senderEmail = new MailAddress("musamasood@gmail.com", "Demo");
-                var receiverEmail = new MailAddress(receiver, "Receiver");
-                var password = "Um9-5Tz)(,5tanx";
-                    var sub = subject;
-                    string body = message;
+                if (ModelState.IsValid)
+                {
+                    var senderEmail = new MailAddress("musamasood1122@gmail.com", "Demo");
+                    var receiverEmail = new MailAddress("ussama.ahmed05@gmail.com");
+                    var password = "Um9-5Tz)(,5tanx";
+                    var sub = Subject;
+                    var body = Message;
+                    var name = Name;
+                    var mail = FromEmail;
+                    var ph=Phone;
 
                     var smtp = new SmtpClient
-                {
+                    {
                         Host = "smtp.gmail.com",
-                    Port = 587,
-                    EnableSsl = true,
-                    DeliveryMethod = SmtpDeliveryMethod.Network,
-                    Credentials = new NetworkCredential(senderEmail.Address, password)
-                };
-
+                        Port = 587,
+                        EnableSsl = true,
+                        DeliveryMethod = SmtpDeliveryMethod.Network,
+                        UseDefaultCredentials = false,
+                        Credentials = new NetworkCredential(senderEmail.Address, password)
+                    };
                     using (var mess = new MailMessage(senderEmail, receiverEmail)
-                {
-                    Subject = (string)subject,
-                    Body = body,
-                })
-                {
-                    smtp.Send(mess);
+                    {
+                        Subject = Subject,
+                        Body = body
+                        
+                    })
+                    {
+                        smtp.Send(mess);
+                    }
+                //    ViewBag.Index = "Sucessfully sent";
+                    return RedirectToAction("Index");
                 }
-                  ViewBag.Index = "Sucessfully sent";
-                return RedirectToAction("Index");
-            }
             }
             catch (Exception)
             {
-                ViewBag.Error = "Some Error";
+                //ViewBag.Error = "Some Error";
                 return RedirectToAction("Some Error");
             }
             return View();
-
-
         }
+
+
 
         private new ActionResult View(object model)
         {
